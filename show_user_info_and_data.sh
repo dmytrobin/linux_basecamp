9854d503ba63026f5748aace8133db5cfffdb523
dmytro <dmytro.binyk@gmail.com>
2022-01-24 04:49:15 +0200

    feat: check if root

--- show_user_info_and_data.sh
+++ show_user_info_and_data.sh
@@ -2,13 +2,15 @@
 USERNAME="$1"
 DIR_PATH="$2"
 str="is found!"
-#printf "%s\t | %s\n" "`find "$DIR_PATH" -user "$USERNAME"`"
 
- find "$DIR_PATH" -user "$USERNAME" | while read line; do
- echo "$line" "$str"
-   #printf %s\ %s\n "$line""$str"
-
-done
-printf "%s\n" "--"
-printf "%s\t%s\n" `ps -u "$USERNAME"`
+if [ "$USERNAME" != "root" ]; then
+  find "$DIR_PATH" -user "$USERNAME" | while read line; do
+  echo "$line" "$str"
+  done
+  printf "%s\n" "--"
+  printf "%s\t%s\n" `ps -u "$USERNAME"`
 exit 0
+else
+  echo "Finding 'root' user data is not allowed!"
+  exit 47
+fi
