c2208b1572126432409fbf157193540576cefe24
dmytro <dmytro.binyk@gmail.com>
2022-01-24 02:30:59 +0200

    feat: implement basic search

--- show_user_info_and_data.sh
+++ show_user_info_and_data.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
-USERNAME = $1
-DIR_PATH = $2
+#!/bin/sh
+USERNAME="$1"
+DIR_PATH="$2"
 
+#echo `find "$DIR_PATH" -user "$USERNAME" 2>&1 | grep -v "Permission denied"`
 
+echo `find "$DIR_PATH" -user "$USERNAME"`
+echo "is found \n"
 exit 0
