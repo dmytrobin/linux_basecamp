6730a2c383cf94cd33850d688d9ce6dd2bf08af2
dmytro <dmytro.binyk@gmail.com>
2022-01-24 04:28:01 +0200

    fix: file search result

--- show_user_info_and_data.sh
+++ show_user_info_and_data.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 USERNAME="$1"
 DIR_PATH="$2"
+str="is found!"
+#printf "%s\t | %s\n" "`find "$DIR_PATH" -user "$USERNAME"`"
 
-printf "%s\n" `find "$DIR_PATH" -user "$USERNAME"`
+ find "$DIR_PATH" -user "$USERNAME" | while read line; do
+ echo "$line" "$str"
+   #printf %s\ %s\n "$line""$str"
 
+done
+printf "%s\n" "--"
 printf "%s\t%s\n" `ps -u "$USERNAME"`
 exit 0
